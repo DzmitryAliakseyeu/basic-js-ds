a45eba307c0149a601265de711bce94ce80414c7
DzmitryAlekseev <dmitry.alekseev.s@gmail.com>
2025-03-01 13:01:46 +0100

    feat: implementation root and add methods for BST DS (Sat, Mar 01, 2025 13:01:32 2 PM)

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,21 +1,51 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
-class BinarySearchTree {
 
-  root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+class BinarySearchTree {
+  constructor(){
+    this.roots = null;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  root() {
+    if(!this.root){
+      return 
+    }
+    return this.roots
+  }
+
+  add(data) {
+    
+    let newNode = new Node(data);
+    if(!this.roots){
+        this.roots = newNode
+        return
+    }
+    
+    let current = this.roots;
+  
+    while(true){
+        if(data < current.data){
+            if(!current.left){
+                current.left = newNode;
+              return 
+            }
+            current = current.left
+      
+        } else {
+            if(!current.right){
+                current.right = newNode
+                return 
+            }
+            current = current.right
+        
+        }
+    }
   }
 
   has(/* data */) {
